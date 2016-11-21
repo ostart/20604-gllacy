7132fc9bb1e2b8595675d47e36cc07400b1b9600
Artem Osta <ostart@mail.ru>
2016-11-21 18:30:53 +0300

    Очередная проверка
    Поправил замечания, хочу новые

--- js/script.js
+++ js/script.js
@@ -29,3 +29,21 @@ cart.addEventListener("mouseout", function(event) {
 		cartOrder.classList.remove("showed");
 	}
 });
+
+
+var mapButton = document.querySelector(".map-button");
+var overlay = document.querySelector(".modal-overlay");
+var modalContent = document.querySelector(".modal-content");
+var close = document.querySelector(".modal-content-close");
+
+mapButton.addEventListener("click", function(event) {
+	event.preventDefault();
+	overlay.classList.add("showed");
+	modalContent.classList.add("showed");
+});
+
+close.addEventListener("click", function(event) {
+	event.preventDefault();
+	overlay.classList.remove("showed");
+	modalContent.classList.remove("showed");
+});
