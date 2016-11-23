7f3a5f7a2a5cfc7f59a7dd610847b1fd17668e3f
Artem Osta <ostart@mail.ru>
2016-11-23 17:55:58 +0300

    Финал 3

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
-var login = document.querySelector(".login");
-var cart = document.querySelector(".cart");
-var loginForm = document.querySelector(".login-form");
-var cartOrder = document.querySelector(".cart-order");
+// var login = document.querySelector(".login");
+// var cart = document.querySelector(".cart");
+// var loginForm = document.querySelector(".login-form");
+// var cartOrder = document.querySelector(".cart-order");
 
-login.addEventListener("mouseover", function(event) {
-    event.preventDefault();
-    loginForm.classList.add("showed");
-});
+// login.addEventListener("mouseover", function(event) {
+//     event.preventDefault();
+//     loginForm.classList.add("showed");
+// });
 
-login.addEventListener("mouseout", function(event) {
-    event.preventDefault();
-    loginForm.classList.remove("showed");
-});
+// login.addEventListener("mouseout", function(event) {
+//     event.preventDefault();
+//     loginForm.classList.remove("showed");
+// });
 
 
-cart.addEventListener("mouseover", function(event) {
-    event.preventDefault();
-    if (cart.classList.contains("cart-full")) {
-        cartOrder.classList.add("showed");
-    }
-});
+// cart.addEventListener("mouseover", function(event) {
+//     event.preventDefault();
+//     if (cart.classList.contains("cart-full")) {
+//         cartOrder.classList.add("showed");
+//     }
+// });
 
-cart.addEventListener("mouseout", function(event) {
-    event.preventDefault();
-    if (cart.classList.contains("cart-full")) {
-        cartOrder.classList.remove("showed");
-    }
-});
+// cart.addEventListener("mouseout", function(event) {
+//     event.preventDefault();
+//     if (cart.classList.contains("cart-full")) {
+//         cartOrder.classList.remove("showed");
+//     }
+// });
 
 
 var mapButton = document.querySelector(".map-button");
