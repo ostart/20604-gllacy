f134ff99312d87f25afa0d768795a1e206a7c77a
Artem Osta <ostart@mail.ru>
2016-11-24 11:57:44 +0300

    Финал 5

--- js/script.js
+++ js/script.js
@@ -1,34 +1,3 @@
-// var login = document.querySelector(".login");
-// var cart = document.querySelector(".cart");
-// var loginForm = document.querySelector(".login-form");
-// var cartOrder = document.querySelector(".cart-order");
-
-// login.addEventListener("mouseover", function(event) {
-//     event.preventDefault();
-//     loginForm.classList.add("showed");
-// });
-
-// login.addEventListener("mouseout", function(event) {
-//     event.preventDefault();
-//     loginForm.classList.remove("showed");
-// });
-
-
-// cart.addEventListener("mouseover", function(event) {
-//     event.preventDefault();
-//     if (cart.classList.contains("cart-full")) {
-//         cartOrder.classList.add("showed");
-//     }
-// });
-
-// cart.addEventListener("mouseout", function(event) {
-//     event.preventDefault();
-//     if (cart.classList.contains("cart-full")) {
-//         cartOrder.classList.remove("showed");
-//     }
-// });
-
-
 var mapButton = document.querySelector(".map-button");
 var overlay = document.querySelector(".modal-overlay");
 var modalContent = document.querySelector(".modal-content");
